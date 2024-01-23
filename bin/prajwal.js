57cea548994877c497578639f28be671bbb82a32
Santhosh DK <vijaysanthu15@gmail.com>
2024-01-23 18:23:23 +0530

    patch

--- bin/prajwal.js
+++ bin/prajwal.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone https://github.com/santhoshdk23/create-prajwal-app.git ${repoName}`;
-const npmInstallCommand = `cd ${themeName} && npm install`;
+const npmInstallCommand = `cd ${repoName} && npm install`;
 
-console.log(`Creating a new WordPress theme called ${themeName}...`);
+console.log(`Creating a new WordPress theme called ${repoName}...`);
 const { execSync } = require('child_process');
 
 const runCommand = (command) => {
@@ -22,13 +22,13 @@ if (!checkedOut) {
     process.exit(-1);
 }
 
-console.log(`Installing dependencies for ${themeName}...`);
+console.log(`Installing dependencies for ${repoName}...`);
 const installed = runCommand(npmInstallCommand);
 
 if (!installed) {
     console.error('Failed to install dependencies');
     process.exit(-1);
 }
-console.log(`Success! Created ${themeName} at ${process.cwd()}/${themeName}`);
+console.log(`Success! Created ${repoName} at ${process.cwd()}/${repoName}`);
 console.log('Inside that directory, you can run several commands:');
 console.log('  npm run start && npm run build');
