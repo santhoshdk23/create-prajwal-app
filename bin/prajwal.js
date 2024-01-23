36ac1e96df194fbb947fd43cc7f50581742aea42
Santhosh DK <vijaysanthu15@gmail.com>
2024-01-23 18:20:53 +0530

    patch

--- bin/prajwal.js
+++ bin/prajwal.js
@@ -1,2 +1,34 @@
 #! /usr/bin/env node
-console.log('Prajwal is a bitch');
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone https://github.com/santhoshdk23/create-prajwal-app.git ${repoName}`;
+const npmInstallCommand = `cd ${themeName} && npm install`;
+
+console.log(`Creating a new WordPress theme called ${themeName}...`);
+const { execSync } = require('child_process');
+
+const runCommand = (command) => {
+    try {
+        execSync(command, { stdio: 'inherit' });
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, error);
+        return false;
+    }
+    return true;
+};
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) {
+    console.error('Failed to checkout the repo');
+    process.exit(-1);
+}
+
+console.log(`Installing dependencies for ${themeName}...`);
+const installed = runCommand(npmInstallCommand);
+
+if (!installed) {
+    console.error('Failed to install dependencies');
+    process.exit(-1);
+}
+console.log(`Success! Created ${themeName} at ${process.cwd()}/${themeName}`);
+console.log('Inside that directory, you can run several commands:');
+console.log('  npm run start && npm run build');
